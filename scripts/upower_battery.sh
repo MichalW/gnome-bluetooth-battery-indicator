dbd1dc6aa61c9e4c25a1495f34d07fd18dbaa8ef
eirik <eirik-ff@users.noreply.github.com>
2022-03-18 10:55:33 +0100

    Fix upower script to work for 100% charged devices, and switch from using sed to using awk.

--- scripts/upower_battery.sh
+++ scripts/upower_battery.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# sed explaination: https://stackoverflow.com/questions/13534306/how-can-i-set-the-grep-after-context-to-be-until-the-next-blank-line
+# awk explaination: https://stackoverflow.com/questions/13534306/how-can-i-set-the-grep-after-context-to-be-until-the-next-blank-line
 # grep explaination: https://stackoverflow.com/questions/1546711/can-grep-show-only-words-that-match-search-pattern
-percent=$(upower --dump | sed -n "/$1/,/^$/p" | grep -oE '[0-9]{1,2}%')
+percent=$(upower --dump | awk "/$1/" RS= | grep -oE '[0-9]{1,3}%')
 connected=$(bluetoothctl info $1 | grep "Connected" | cut -d ' ' -f2)
 
 if [[ $connected == "yes" ]]; then
