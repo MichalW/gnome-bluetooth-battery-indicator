b4dac0a1e971a53a93db149ff6aa8fe59cfbdff2
Mauro <mfontana@df.uba.ar>
2022-03-20 14:31:34 -0300

    Fixed case mismatch in UPower script

--- scripts/upower_battery.sh
+++ scripts/upower_battery.sh
@@ -2,7 +2,7 @@
 
 # awk explaination: https://stackoverflow.com/questions/13534306/how-can-i-set-the-grep-after-context-to-be-until-the-next-blank-line
 # grep explaination: https://stackoverflow.com/questions/1546711/can-grep-show-only-words-that-match-search-pattern
-percent=$(upower --dump | awk "/$1/" RS= | grep -oE '[0-9]{1,3}%')
+percent=$(upower --dump | tr '[:lower:]' '[:upper:]' | awk "/$1/" RS= | grep -oE '[0-9]{1,3}%')
 connected=$(bluetoothctl info $1 | grep "Connected" | cut -d ' ' -f2)
 
 if [[ $connected == "yes" ]]; then
