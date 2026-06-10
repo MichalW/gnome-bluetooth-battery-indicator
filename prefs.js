17fbc298ce2a9f57404443d8e7e1d42d9094ae36
Michał Wanat <michalwanat@gmail.com>
2026-06-10 10:17:05 +0200

    fixes

--- prefs.js
+++ prefs.js
@@ -1,12 +1,20 @@
+import Adw from 'gi://Adw';
 import {ExtensionPreferences} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 import SettingsWidget from './settingsWidget.js';
 import {SettingsController} from './settings.js';
 
 export default class BluetoothBatteryIndicatorExtensionPreferences extends ExtensionPreferences {
-    getPreferencesWidget() {
+    fillPreferencesWindow(window) {
         const settingsController = new SettingsController(this.getSettings());
 
-        return new SettingsWidget(settingsController);
+        const page = new Adw.PreferencesPage();
+        const group = new Adw.PreferencesGroup();
+        page.add(group);
+
+        const settingsWidget = new SettingsWidget(settingsController);
+        group.add(settingsWidget);
+
+        window.add(page);
     }
 }
